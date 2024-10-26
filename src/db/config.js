d52439f9ec63fbfb9d9e0ab2552a770c312530b2
Md. Aidul Islam <aidul5613@gmail.com>
2024-10-26 21:27:13 +0300

    utils added

--- src/db/config.js
+++ src/db/config.js
@@ -5,15 +5,14 @@ dotenv.config();
 
 const { DB_NAME } = require("../constants");
 
-
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
 
-    console.log(`mongodb connected`);
-    console.log(`mongodb host ${connectionInstance.connection.host}`);
+    console.log(`✔️  mongodb connected`);
+    console.log(`✔️  mongodb host ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error("ERROR: ", error);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-require("dotenv").config({path: "../.env"});
-
-const mongoose = require("mongoose");
-const express = require("express");
+require("dotenv").config({ path: "../.env" });
+const { app } = require("./app");
 const connectDB = require("./db/config");
 
-const app = express();
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`✔️  server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`❌  Mongodb connection failed!!`, error);
+  });
 
 //We can also connect db this way
 // (async () => {
